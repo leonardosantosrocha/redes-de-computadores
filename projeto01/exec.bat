bd242e90680803b4dcf2db019b069921d6d3eaed
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-03-24 08:53:25 -0300

    Projeto 01 - Exec
    Orquestrador para execução do script "clienteServidor.py" do Projeto 01.

--- projeto01/exec.bat
+++ projeto01/exec.bat
@@ -1,3 +1,3 @@
 start /min cmd /c python clienteServidor.py
-start /min cmd /c python clienteServidor.py 10.0.0.106
-start /min cmd /c python clienteServidor.py 10.0.0.106
+start /min cmd /c python clienteServidor.py "SEU_IPV4"
+start /min cmd /c python clienteServidor.py "SEU_IPV4"
