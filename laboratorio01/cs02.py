c1952514dd3dd645502ac67bc16d5eaa0e61cc90
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-03-23 21:23:27 -0300

    Cliente e Servidor 2
    Código utilizado para representar a comunicação entre dois clientes/servidores através do protocolo UDP.

---
+++ laboratorio01/cs02.py
@@ -0,0 +1,35 @@
+# Cliente/Servidor 2
+
+# Importando os módulos
+import socket
+from threading import Thread
+
+# Servidor
+def servidor():
+    ip_servidor = "SEU_IPV4"
+    porta_servidor = 20000
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+
+    sock.bind((ip_servidor, porta_servidor)) 
+     
+    while True:
+        pacote, endereco = sock.recvfrom(1024)
+        if (pacote.decode('utf-8') == "QUIT"):
+            sock.close()
+            break
+        print(f"[Cliente 2]: {pacote.decode('utf-8')}")
+
+# Cliente
+def cliente():
+    ip_destino = "SEU_IPV4"
+    porta_destino = 15000
+
+    while True:
+        mensagem = str(input(">> "))
+        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        sock.sendto(mensagem.encode('UTF-8'), (ip_destino, porta_destino))
+
+if __name__ == '__main__':
+    Thread(target = servidor).start()
+    Thread(target = cliente).start()
